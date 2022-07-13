722601d32a47cbea5a4c7e0a00dfc84fd6d4f5bd
Lokeswari1999 <108974119+Lokeswari1999@users.noreply.github.com>
2022-07-13 15:02:52 +0530

    Create PROGRAM-4.c

---
+++ PROGRAM-4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    long int rem,num,cnt=0;
+    printf("enter lottery tickets ");
+    scanf("%ld",&num);
+    label:
+    if(num!=0)
+    {
+        rem=num%10;
+        if(rem==3)
+        cnt++;
+        num/=10;
+        goto label;
+    }
+    if(cnt>0)
+    printf("%ld",cnt);
+}
+/*#include <stdio.h>
+int main()
+{
+	int a[]={1,3,4,5,6,3,23,6,63,43,53};
+	int n=sizeof(a)/sizeof(a[0]);
+	int i,count=0;
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==3)
+			count++;
+	}
+	printf("%d",count);
+} 
+*/
